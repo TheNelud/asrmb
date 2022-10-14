ba1a16d07b213132b1a01e134f542388bc97e03c
TheNelud <riwok@bk.ru>
2022-10-14 15:09:25 +0300

    Добавил функцию обновления

--- static/journal/js/update_content.js
+++ static/journal/js/update_content.js
@@ -9,13 +9,25 @@ function showDiv(id) {
   }
 }
 
+function showDivContent(id_content){
+  var all = document.querySelectorAll(".oks_content_text");
+  for (var i = 0; i < all.length; i++) {
+    if (all[i].id === id_content) {
+      all[i].style.display = (all[i].style.display === 'none')? 'block' : 'none';
+    } else {
+      all[i].style.display = 'none';
+    }
+  }
+}
+
 async function updateDivContent(selector) {
   try {
-    document.querySelector(selector).style.display = "block";
     var html = await (await fetch(location.href)).text();
     var newdoc = new DOMParser().parseFromString(html, 'text/html');
     document.querySelector(selector).outerHTML = newdoc.querySelector(selector).outerHTML;
     console.log('Элемент '+selector+' был успешно обновлен');
+    showDivContent(selector);
+    document.querySelector(selector).style.display="block";
     return true;
   } catch(err) {
     console.log('При обновлении элемента '+selector+' произошла ошибка:');
