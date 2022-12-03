3c6827854b26271212d454a269b6ce13273d15bb
TheNelud <riwok@bk.ru>
2022-12-03 20:57:20 +1100

    добавил нормальные графики в суточный отчет, доделать стили

--- journal_rtp/forms.py
+++ journal_rtp/forms.py
@@ -1,25 +1,78 @@
 from django.forms import ModelForm
 from .models import *
+from django import forms
 
 class TeclossesOneForm(ModelForm):
+    name = forms.CharField(label="Оборудование", max_length=255)
+    v = forms.FloatField(label="V - геометрический объем аппарата")
+    pn = forms.FloatField(label="Рн - абсолютное давление природного газа перед началом ремонтной работы")
+    tn = forms.FloatField(label="Тн - температура природного газа перед началом работы")
+    zn = forms.FloatField(label="Zн - коэффициенты сверхсжимаемости природного газа")
+    press_pk = forms.FloatField(label="Рк - абсолютное давление природного газа после опорожнения")
+    tk = forms.FloatField(label="Тк - температура природного газа после опорожнения")
+    zk = forms.FloatField(label="Zк - коэффициенты сверхсжимаемости природного газа")
+    # v_op = models.FloatField()
+    # ng_prod = models.FloatField()
+    ng_nl = forms.FloatField(label="nг.пл - соответственно число молей в газе")
+    # xg_prod = models.FloatField()
+    n = forms.FloatField(label="N - количество операций в расчетный период")
+    # pn_op = forms.FloatField(label="соответственно число молей в газе")
+    mol = forms.FloatField(label="Мольная доля С1-С4")
     class Meta:
         model = TeclossesOne
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['name','v','pn','tn','zn','press_pk','tk','zk','ng_nl','n','mol']
 
 class TeclossesTwoForm(ModelForm):
+    name = forms.CharField(label="Оборудование", max_length=255)
+    qgr_sh = forms.FloatField(label="Qг.р.ж. - расход природного газа при регенерации технических жидкостей")
+    ng_prod = forms.FloatField(label="nг.пл - соответственно число молей в газе")
+    ng_pl = forms.FloatField(label='nг.прод - соответственно число молей в газе')
+    # xg_prod = forms.FloatField(label='Xг.прод - мольная доля добываемой продукции в газе')
+    # pgr_sh = forms.FloatField(label="Пг.р.ж. - потери природного газа при регенерации технических жидкостей")
     class Meta:
         model = TeclossesTwo
-        fields = '__all__'
+        fields = ['name', 'qgr_sh', 'ng_prod', 'ng_pl']
 
 class TeclossesTreeForm(ModelForm):
+    name = forms.CharField(label="Вид анализа", max_length=255)
+    v_pr = forms.FloatField(label="Vпр - геометрический объем пробоотборника для анализа i-го вида")
+    p_pr = forms.FloatField(label="Рпр - давление в пробоотборнике для анализа i-го вида")
+    t_pr = forms.FloatField(label="Tпр - температура в пробоотборнике для анализа i-го вида")
+    z_pr = forms.FloatField(label="Zпр - коэффициент сжимаемости газа")
+    b = forms.FloatField(label="b -кратность продувки, т.е. отношение объема (при условии отбора) газа,")
+    ni = forms.FloatField(label="ni")
+    xr_prod = forms.FloatField(label="Xг.прод - мольная доля добываемой продукции в газе")
+    # pr_op = forms.FloatField(label="Пг.оп - потери природного газа при периодическом отборе проб для разовых")
+    device = forms.CharField(label="Приборы",max_length=255)
+    # v_p = forms.FloatField(label="Vр.- расход газа на i-й прибор")
+    tau = forms.FloatField(label="τр - время работы i-го прибора в расчетный период")
+    xrr_prod = forms.FloatField(label="Xг.прод - мольная доля добываемой продукции в газе")
+    n = forms.FloatField(label="n - число видов анализов")
+    # pr_pot = forms.FloatField(label="Пг.пот.i - потери природного газа при работе i-го прибора на потоке")
+    # pr_pr = forms.FloatField(label="Пг.пр - соответственно число молей пластового газа и газа")
     class Meta:
         model = TeclossesTree
-        fields = '__all__'
+        fields = ['name','v_pr','p_pr','t_pr','z_pr','b','ni','xr_prod','device','tau','xrr_prod','n']
 #________________________________________________________________________________
 class RecyclingcalcOneForm(ModelForm):
+    type = forms.CharField(label="Тип", max_length=255)
+    aij = forms.FloatField(label="Аij - величина j-ого потока через одно уплотнение")
+    bij = forms.FloatField(label="bij - количество уплотнений i-го типа на j-м потоке газа")
+    tauij = forms.FloatField(label="τij - время работы уплотнений i-го типа на j-м потоке газа в расчетный период")
+    a_ij = forms.FloatField(label="аij - расчетная доля уплотнений i-го типа на j-м потоке газа в расчетный период")
+    mi = forms.FloatField(label="Mj - молярная масса газа в j-м потоке")
+    # q_yp = forms.FloatField(label="")
+    t_type = forms.CharField(label="Тип", max_length=255)
+    t_aij = forms.FloatField(label="Аij - величина j-ого потока через одно уплотнение")
+    t_bij = forms.FloatField(label="bij - количество уплотнений i-го типа на j-м потоке газа")
+    t_tauij = forms.FloatField(label="τij - время работы уплотнений i-го типа на j-м потоке газа в расчетный период")
+    t_a_ij = forms.FloatField(label="аij - расчетная доля уплотнений i-го типа на j-м потоке газа в расчетный период")
+    t_mi = forms.FloatField(label="Mj - молярная масса газа в j-м потоке")
+    t_q_yp = forms.FloatField(label="")
     class Meta:
         model = RecyclingcalcOne
-        fields = '__all__'
+        fields = ['type','aij','bij','tauij','a_ij', 'mi']
 
 class RecyclingcalcTwoForm(ModelForm):
     class Meta:
--- raports/views.py
+++ raports/views.py
@@ -6,19 +6,217 @@ import pandas as pd
 import numpy as np
 
 
+from plotly.offline import plot
+
+
+
 def graphs_P_pcv():
 
     N =30
     random_x = np.linspace(0, 1, N)
     random_y0 = np.random.randn(N) + 5
    
-    figure = go.Figure(px.line(x=random_x,
-                                 y=random_y0
-                                 )).to_html(config= {'displaylogo': False})
-    
+    layout = go.Layout(
+        # title_text='States_Name',
+        yaxis2=dict(overlaying='y'),
+        width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=0)
+    )
+
+    figure = go.Figure([go.Scatter(x = random_x,y = random_y0,name='P4 после PCV'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='P5 после PCV'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='P3 после PCV'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='P4-бис после PCV'),
+        go.Scatter(x = random_x,y = random_y0 + 1, name='P3 после PCV')],
+        layout=layout).to_html({'displaylogo': False})
+    # figure.to_html(config= {'displaylogo': False})
     return figure
 
 
+def graphs_T_c():
+    N =30
+    random_x = np.linspace(0, 1, N)
+    random_y0 = np.random.randn(N) + 5
+   
+    layout = go.Layout(
+        # title_text='States_Name',
+        yaxis2=dict(overlaying='y'),
+        width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=0)
+    )
+
+    figure = go.Figure([go.Scatter(x = random_x,y = random_y0,name='Температура точки росы'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Т норма УВ'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Температура УВ'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Содержание (СО2), %'),
+        go.Scatter(x = random_x,y = random_y0 + 1, name='Т норма Н2О'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='СО2 норма')],
+        layout=layout).to_html({'displaylogo': False})
+    
+    return figure
+
+def graphs_V(): 
+    
+    data = {'50Р-1№1':np.linspace(1, 100, 1),
+            '50Р-1№2':np.linspace(1, 100, 1)}
+    courses = list(data.keys())
+    print(courses)
+    values = list(data.values())
+    print(values)
+
+    figure = go.Figure(px.bar(data,
+                                width=500,
+                                height=240,
+                                )).to_html(config= {'displaylogo': False})
+
+    
+    return figure
+
+#__________________________________________#
+def graphs_1():
+    N =30
+    random_x = np.linspace(0, 30, N)
+    random_y0 = np.random.randn(N) + 5
+
+
+    layout = go.Layout(
+        title_text='БАЛАНСОВЫЕ И ПЛАНОВЫЕ ПОКАЗАТЕЛИ ДОБЫЧИ ГАЗА (тыс. м3/сут)',
+        yaxis2=dict(overlaying='y'),
+        # width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=30)
+    )
+
+    figure = go.Figure([go.Scatter(x = random_x,y = random_y0,name='Добыча газа план'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Добыча газа факт'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Добыча газа P5'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='обыча газа Р6'),
+        go.Scatter(x = random_x,y = random_y0 + 1, name='Добыча газа Р3'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Добыча газа Р4-бис')],
+        layout=layout).to_html({'displaylogo': False})
+
+     #('БАЛАНСОВЫЕ И ПЛАНОВЫЕ ПОКАЗАТЕЛИ ДОБЫЧИ ГАЗА (тыс. м3/сут)')
+                                 
+    return figure
+
+def graphs_2():
+    N =30
+    random_x = np.linspace(0, 30, N)
+    random_y0 = np.random.randn(N) + 5
+
+    layout = go.Layout(
+        title_text='ДОБЫЧА И ПЕРЕРАБОТКА КОНДЕНСАТА ГАЗОВОГО (тыс.тонн/сут)',
+        yaxis2=dict(overlaying='y'),
+        # width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=30)
+    )
+
+    figure = go.Figure([go.Scatter(x = random_x,y = random_y0,name='Добыча КГН, тыс. тонн'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Производство КГС, тыс. тонн'),
+        go.Scatter(x = random_x,y = random_y0 - 0.5, name='Добыча КГН план, тыс. тонн'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Производство КГС план, тыс. тонн')],
+        layout=layout).to_html({'displaylogo': False})
+    return figure
+
+def graphs_3():
+    N =30
+    random_x = np.linspace(0, 30, N)
+    random_y0 = np.random.randn(N) + 5
+   
+    layout = go.Layout(
+        title_text='ПОСТАВКА КОНДЕНСАТА ГАЗОВОГО (тыс.тонн/сут)',
+        yaxis2=dict(overlaying='y'),
+        # width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=30)
+    )
+
+    figure = go.Figure([go.Bar(x = random_x,y = random_y0,name='Добыча КГН, тыс. тонн'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Накопленная откачка в СЭИК'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='План откачки КГС в СЭИК'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='ППлан откачки КГС за месяц, тыс. тонн')],
+        layout=layout).to_html({'displaylogo': False})
+    return figure
+
+def graphs_4():
+    N =30
+    random_x = np.linspace(0, 1, N)
+    random_y0 = np.random.randn(N) + 5
+   
+    layout = go.Layout(
+        title_text='Потребление газа в г. Владивосток (тыс.м3/сут), давление на входе ГКС "Сахалин" (МПа)',
+        yaxis2=dict(overlaying='y'),
+        # width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=30)
+    )
+
+    figure = go.Figure([go.Bar(x = random_x,y = random_y0,name='Потребление газа на г.Владивосток'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Р (МПа), на входе в ГКС Сахалин')],
+        layout=layout).to_html({'displaylogo': False})
+    return figure
+
+def graphs_5():
+    N =30
+    random_x = np.linspace(0, 1, N)
+    random_y0 = np.random.randn(N) + 5
+   
+    layout = go.Layout(
+        title_text='ПОСТУПЛЕНИЕ ЖИДКОСТИ НА УКПГ (тонн/сут)',
+        yaxis2=dict(overlaying='y'),
+        # width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=30)
+    )
+
+    figure = go.Figure([go.Bar(x = random_x,y = random_y0,name='Рефлюксная вода с УРМ'),
+        go.Bar(x = random_x,y = random_y0 + 0.2,name='Поступление КГН'),
+        go.Bar(x = random_x,y = random_y0 - 0.3 ,name='Поступление нМЭГ')],
+        layout=layout).to_html({'displaylogo': False})
+    return figure
+
+def graphs_6():
+    N =30
+    random_x = np.linspace(0, 1, N)
+    random_y0 = np.random.randn(N) + 5
+   
+    layout = go.Layout(
+        title_text='СТН И ПОТЕРИ ГАЗА (тыс. м3/сут)',
+        yaxis2=dict(overlaying='y'),
+        # width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=30)
+    )
+
+    figure = go.Figure([go.Scatter(x = random_x,y = random_y0 + 0.5, name='Собственные тех. нужды'),
+        go.Scatter(x = random_x,y = random_y0 - 0.5, name='Потери')],
+        layout=layout).to_html({'displaylogo': False})
+    return figure
+
+def graphs_7():
+    N =30
+    random_x = np.linspace(0, 1, N)
+    random_y0 = np.random.randn(N) + 5
+   
+    layout = go.Layout(
+        title_text='МАТЕРИАЛЬНЫЙ БАЛАНС ХИМ. РЕАГЕНТОВ (РАСТВОР МЭГ) (тонн/сут)',
+        yaxis2=dict(overlaying='y'),
+        # width=800,
+        height=300,
+        margin=dict(l=0,r=0,b=0,t=30)
+    )
+
+    figure = go.Figure([go.Bar(x = random_x,y = random_y0,name='Концентрация нМЭГ'),
+        go.Bar(x = random_x,y = random_y0 + 0.2,name='Концентрация рМЭГ'),
+        go.Scatter(x = random_x,y = random_y0 + 0.5, name='Расход рМЭГ'),
+        go.Scatter(x = random_x,y = random_y0 - 0.5, name='Регенерация МЭГ'),
+        go.Scatter(x = random_x,y = random_y0 + 0.3, name='Приход нМЭГ на УРМ')],
+        layout=layout).to_html({'displaylogo': False})
+    return figure
+#___________________________________________#
 # Create your views here.
 def mar(request):
    
@@ -36,9 +234,30 @@ def sar(request):
 def sr_kgmk(request):
 
     graphsPpcv = graphs_P_pcv()
+    graphsTc = graphs_T_c()
+    graphsV = graphs_V()
 
+    _graphs_1 = graphs_1()
+    _graphs_2 = graphs_2()
+    _graphs_3 = graphs_3()
+    _graphs_4 = graphs_4()
+    _graphs_5 = graphs_5()
+    _graphs_6 = graphs_6()
+    _graphs_7 = graphs_7()
+    
+    
     context = {
         'graphs_P_pcv':graphsPpcv,
+        'graphs_T_c':graphsTc,
+        'graphs_V':graphsV,
+
+        'graphs_1':_graphs_1,
+        'graphs_2':_graphs_2,
+        'graphs_3':_graphs_3,
+        'graphs_4':_graphs_4,
+        'graphs_5':_graphs_5,
+        'graphs_6':_graphs_6,
+        'graphs_7':_graphs_7,
     }
 
     return render(request, 'sr_kgmk.html', context)
