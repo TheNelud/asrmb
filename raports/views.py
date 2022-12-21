639bed2aac433aac8db922d08e7e97f6e48939e6
TheNelud <riwok@bk.ru>
2022-12-21 10:14:15 +0300

    добавил изменение в дату -1 день для печати в сэр

--- raports/views.py
+++ raports/views.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.http import JsonResponse
 from .calculate_balance import calculate_balance
 import datetime
+from datetime import timedelta
 
 
 from django.views.generic.edit import UpdateView
@@ -301,9 +302,10 @@ def mag(request):
 
 def filter_date_mag(request):
     max_date_now = datetime.datetime.now().strftime("%Y-%m-%d")
+    
     items_tech = Sen_equip.objects.filter(date_update__contains=request.POST.get('date_update','')).order_by('-date_update')[:1] 
     items_balance =Balance.objects.filter(date_update__contains=request.POST.get('date_update','')).order_by('-date_update')[:1]
-
+    
     return render(request, 'mag.html', context={
                                                "itemss_tech":items_tech,
                                                 "itemss_balance":items_balance,
@@ -383,9 +385,16 @@ def filter_date_sar(request):
     max_date_now = datetime.datetime.now().strftime("%Y-%m-%d")
     items_day = Ser_per_day.objects.filter(date_update__contains=request.POST.get('date_update','')).order_by('-id')[:1]
     items_month =Ser_per_month.objects.filter(date_update__contains=request.POST.get('date_update','')).order_by('-id')[:1]
+    if items_day.values():
+        just_day = items_day.values()[0]['date_update']
+        delta_day = (just_day - timedelta(days=1)).strftime("%d.%m.%Y")
+    else:
+        delta_day = "-"
+     
     return render(request, 'sar.html', context={'items_ser_day' : items_day,
                                                 'items_ser_month' : items_month,
-                                                'max_date_now':max_date_now
+                                                'max_date_now':max_date_now,
+                                                'delta_day': delta_day
                                                 }) 
 
 
